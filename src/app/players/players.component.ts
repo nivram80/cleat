f390660c289f90dbd7d7b532b6ef62066b0e187b
Todd Nichols <nivram80@gmail.com>
2017-03-24 20:22:40 -0500

    add player module

---
+++ src/app/players/players.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player',
+  templateUrl: 'players.component.html',
+  styleUrls: ['players.component.css']
+})
+export class PlayersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
